4fa239cbec6c9e45ba8fbce3d81b05f5726053c5
ns24-ctrl <srivastavanirbhay900@gmail.com>
2026-08-08 17:34:43 +0530

    Add MainActivity to load YouTube in WebView

---
+++ app/src/main/app/src/main/java/com/freestreamhub/MainActivity.java
@@ -0,0 +1,26 @@
+package com.freestreamhub;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.youtube.com/");
+
+        setContentView(webView);
+    }
+}
